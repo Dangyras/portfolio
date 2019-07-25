c5d7ec9691abfdc2f717dafd5c64215551e63543
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-25 18:23:15 +0300

    Added new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
+import Main from "./components/layout/Main";
+import Footer from "./components/layout/Footer";
 import "./App.css";
 
-function App() {
-    return (
-        <div className="App">
-          <h1>Test</h1>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Main />
+                <Footer />
+            </div>
+        );
+    }
 }
+
 export default App;
