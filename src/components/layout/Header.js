d9beb1ed6a329499af750dc83f52a2d52388eb03
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-25 21:50:00 +0300

    Added content & style to desktop header.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,41 @@
 import React from "react";
+import logo from "../../logo.svg";
+import phone from "../../phone.svg";
+import email from "../../email.svg";
+import doc from "../../document.svg";
 
 export default function Header() {
     return (
         <header>
-            <h1>Header</h1>
+            <div className="section announcement-section">
+                <div className="container" />
+            </div>
+            <div className="section header-section">
+                <div className="container">
+                    <div className="navigation-wrapper">
+                        <div className="logo">
+                            <img src={logo} className="logo" alt="logo" draggable="false" />
+                        </div>
+                        <nav>
+                            <ul>
+                                <li>darbai</li>
+                                <li>apie mane</li>
+                            </ul>
+                            <ul class="menu-icons">
+                                <li>
+                                    <img src={phone} className="logo" alt="logo" draggable="false" />
+                                </li>
+                                <li>
+                                    <img src={email} className="logo" alt="logo" draggable="false" />
+                                </li>
+                                <li>
+                                    <img src={doc} className="logo" alt="logo" draggable="false" />
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
