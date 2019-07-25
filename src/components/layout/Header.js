6679cafaac0fc77989b473eebcdbff21e9c77bc1
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-25 23:02:32 +0300

    Added mobile menu button function

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,46 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../../logo.svg";
 import phone from "../../phone.svg";
 import email from "../../email.svg";
 import doc from "../../document.svg";
 
-export default function Header() {
-    return (
-        <header>
-            <div className="section announcement-section">
-                <div className="container" />
-            </div>
-            <div className="section header-section">
-                <div className="container">
-                    <div className="navigation-wrapper">
-                        <div className="logo">
-                            <img src={logo} className="logo" alt="logo" draggable="false" />
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
+
+    toggleMenu() {
+        const currentState = this.state.active;
+        this.setState({ active: !currentState });
+    }
+
+    render() {
+        return (
+            <header className={this.state.active ? "menu-visible" : null}>
+                <div className="section announcement-section">
+                    <div className="container" />
+                </div>
+                <div className="section header-section">
+                    <div className="container">
+                        <div className="navigation-wrapper">
+                            <div className="logo">
+                                <img src={logo} className="logo" alt="logo" draggable="false" />
+                            </div>
+                            <nav>
+                                <ul>
+                                    <li>darbai</li>
+                                    <li>apie mane</li>
+                                </ul>
+                                <ul className="menu-icons">
+                                    <li>
+                                        <img src={phone} className="logo" alt="logo" draggable="false" />
+                                    </li>
+                                    <li>
+                                        <img src={email} className="logo" alt="logo" draggable="false" />
+                                    </li>
+                                    <li>
+                                        <img src={doc} className="logo" alt="logo" draggable="false" />
+                                    </li>
+                                </ul>
+                            </nav>
+                            <button id="mobile-nav-toggle" className="nav-toggle" onClick={() => this.toggleMenu()}>
+                                <span />
+                                <span className="nav-toggle-middle" />
+                                <span />
+                            </button>
                         </div>
-                        <nav>
-                            <ul>
-                                <li>darbai</li>
-                                <li>apie mane</li>
-                            </ul>
-                            <ul class="menu-icons">
-                                <li>
-                                    <img src={phone} className="logo" alt="logo" draggable="false" />
-                                </li>
-                                <li>
-                                    <img src={email} className="logo" alt="logo" draggable="false" />
-                                </li>
-                                <li>
-                                    <img src={doc} className="logo" alt="logo" draggable="false" />
-                                </li>
-                            </ul>
-                        </nav>
-                        <button id="mobile-nav-toggle" className="nav-toggle">
-                            <span />
-                            <span className="nav-toggle-middle" />
-                            <span />
-                        </button>
                     </div>
                 </div>
-            </div>
-        </header>
-    );
+            </header>
+        );
+    }
 }
+
+export default Header;
