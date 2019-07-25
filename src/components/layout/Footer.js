17b4c4a6a2b525ad247a40cb744f0fd203a97091
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-25 19:48:03 +0300

    Added content & style to footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,31 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            <h1>Footer</h1>
+            <div className="section menu-section">
+                <div className="container">
+                    <div className="footer-menu">
+                        <h3>Feel free to contact me! I'm on the lookout for full-time work opportunities and I'd love to collaborate.</h3>
+                        <ul className="menu">
+                            <li>
+                                <a href="#">Email</a>
+                            </li>
+                            <li>
+                                <a href="#">Phone</a>
+                            </li>
+                            <li>
+                                <a href="#">Other</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="section copyright-section">
+                <div className="container">
+                    <div className="copyright">
+                        <p>© Dangyras Stepanavicius // 2019</p>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
