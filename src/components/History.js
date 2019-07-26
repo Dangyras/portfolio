a54cef25b2288684b8af09a7c8975d4c306f2437
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-26 15:49:04 +0300

    Added Experience component to homepage.

--- src/components/History.js
+++ src/components/History.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import HistoryList from "./HistoryList";
 
 class History extends Component {
     render() {
         return (
             <section>
                 <div className="container">
-                    <h1>History</h1>
+                    <div className="section-inner">
+                        <h2 className="section-heading">
+                            <span className="heading-styled">Patirtis.</span>
+                            <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic?
+                            <br />
+                        </h2>
+                        <HistoryList />
+                        <h2 className="section-heading heading-right">
+                            <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic?
+                            <br />
+                        </h2>
+                    </div>
                 </div>
             </section>
         );
---
+++ src/components/HistoryList.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class HistoryList extends Component {
+    render() {
+        return (
+            <div className="history-list">
+                <div class="point-wrapper">
+                    <span class="point" />
+                </div>
+                <div class="point-wrapper">
+                    <span class="point" />
+                </div>
+                <div class="point-wrapper">
+                    <span class="point" />
+                </div>
+                <div class="point-wrapper">
+                    <span class="point" />
+                </div>
+                <div class="point-wrapper">
+                    <span class="point" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HistoryList;
