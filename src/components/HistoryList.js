8e20874f050f3ca1bf0addec4e5d18fc4d5c435a
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-29 15:12:48 +0300

    Changed Experience section structure and added some new styling.

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,36 +1,15 @@
 import React, { Component } from "react";
+import ExperienceItem from "./ExperienceItem";
 
 class HistoryList extends Component {
     render() {
         return (
             <div className="history-list">
-                <div className="point-wrapper">
-                    <span className="point shadow" />
-                    <div className="experience-content shadow">
-                        <button className="close" title="Uzdaryti" />
-                        <h3>Lorem, ipsum dolor.</h3>
-                        <span>2000 - 2000</span>
-                        <button className="info" title="Daugiau">
-                            ?
-                        </button>
-                    </div>
-                </div>
-                <div className="point-wrapper">
-                    <span className="point shadow" />
-                    <div className="experience-content shadow" />
-                </div>
-                <div className="point-wrapper">
-                    <span className="point shadow" />
-                    <div className="experience-content shadow" />
-                </div>
-                <div className="point-wrapper">
-                    <span className="point shadow" />
-                    <div className="experience-content shadow" />
-                </div>
-                <div className="point-wrapper">
-                    <span className="point shadow" />
-                    <div className="experience-content shadow" />
-                </div>
+                <ExperienceItem />
+                <ExperienceItem />
+                <ExperienceItem />
+                <ExperienceItem />
+                <ExperienceItem />
             </div>
         );
     }
