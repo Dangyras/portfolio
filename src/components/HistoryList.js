f2c0e9f6baf774bea02fafb3a33ef95a3ffb3589
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-29 11:12:57 +0300

    Added experience tabs to Experience section.

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -6,18 +6,27 @@ class HistoryList extends Component {
             <div className="history-list">
                 <div className="point-wrapper">
                     <span className="point shadow" />
+                    <div className="experience-content shadow">
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <span>2000 - 2000</span>
+                        <span className="info">?</span>
+                    </div>
                 </div>
                 <div className="point-wrapper">
                     <span className="point shadow" />
+                    <div className="experience-content shadow" />
                 </div>
                 <div className="point-wrapper">
                     <span className="point shadow" />
+                    <div className="experience-content shadow" />
                 </div>
                 <div className="point-wrapper">
                     <span className="point shadow" />
+                    <div className="experience-content shadow" />
                 </div>
                 <div className="point-wrapper">
                     <span className="point shadow" />
+                    <div className="experience-content shadow" />
                 </div>
             </div>
         );
