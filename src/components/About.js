8afd7cf5769de5c349048f38ff4871b7d933d5be
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-29 16:17:34 +0300

    Added content & style to About me section.

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,30 @@ class About extends Component {
                             <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic?
                             <br />
                         </h2>
+                        <div className="information-list">
+                            <ul>
+                                <li>
+                                    <span className="name">Vardas</span>
+
+                                    <span className="name">Dangyras Stepanavicius</span>
+                                </li>
+                                <li>
+                                    <span className="name">Miestas</span>
+
+                                    <span className="name">Kaunas | Marijampole</span>
+                                </li>
+                                <li>
+                                    <span className="name">Telefonas</span>
+
+                                    <span className="name">99999</span>
+                                </li>
+                                <li>
+                                    <span className="name">Issilavinimas</span>
+
+                                    <span className="name">VDU</span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </section>
