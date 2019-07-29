b116ac9bd6c5bc707103b9819577176faead17c0
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-07-29 14:13:47 +0300

    Added close & more buttons to experience cards.

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -7,9 +7,12 @@ class HistoryList extends Component {
                 <div className="point-wrapper">
                     <span className="point shadow" />
                     <div className="experience-content shadow">
+                        <button className="close" title="Uzdaryti" />
                         <h3>Lorem, ipsum dolor.</h3>
                         <span>2000 - 2000</span>
-                        <span className="info">?</span>
+                        <button className="info" title="Daugiau">
+                            ?
+                        </button>
                     </div>
                 </div>
                 <div className="point-wrapper">
