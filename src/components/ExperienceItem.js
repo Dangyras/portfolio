ac2147394f88da0a0192e7a4c53ea154c5427517
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-08-07 15:06:45 +0300

    Created expanded experience card.

--- src/components/ExperienceItem.js
+++ src/components/ExperienceItem.js
@@ -4,26 +4,51 @@ class ExperienceItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            active: false
+            card: false,
+            expand: false
         };
     }
 
     toggleCard() {
-        const currentState = this.state.active;
-        this.setState({ active: !currentState });
+        const currentState = this.state.card;
+        this.setState({ card: !currentState });
+    }
+
+    toggleContent() {
+        const currentState = this.state.expand;
+        this.setState({ expand: !currentState });
+        if (!currentState) {
+            document.body.classList.add("no-sroll");
+        } else {
+            document.body.classList.remove("no-sroll");
+        }
     }
 
     render() {
         return (
-            <div className={this.state.active ? "card-visible point-wrapper" : "point-wrapper"}>
+            <div className={this.state.card ? "card-visible point-wrapper" : "point-wrapper"}>
                 <span className="point shadow" onClick={() => this.toggleCard()} />
-                <div className="experience-content shadow">
-                    <button className="close" title="Uzdaryti" onClick={() => this.toggleCard()} />
+                <div className={this.state.expand ? "experience-content shadow open" : "experience-content shadow"}>
+                    <button
+                        className="close"
+                        title="Uzdaryti"
+                        onClick={() => {
+                            this.toggleCard();
+                            this.setState({ expand: false });
+                            document.body.classList.remove("no-sroll");
+                        }}
+                    />
                     <h3>Lorem, ipsum dolor.</h3>
                     <div className="time">
                         <span>2000</span> <span>2000</span>
                     </div>
-                    <button className="info" title="Daugiau">
+                    <button
+                        className="info"
+                        title="Daugiau"
+                        onClick={() => {
+                            this.toggleContent();
+                        }}
+                    >
                         ?
                     </button>
                 </div>
